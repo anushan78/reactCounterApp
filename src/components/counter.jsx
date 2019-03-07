a6f67aae33ace67f533d29a8fb744070379583e7
Anushan Tennakoon <anushan.tennakoon@rxpservices.com>
2019-03-07 23:23:54 +1100

    Initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles = {
+    fontSize: 60,
+    fontWeight: "bold"
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <img src={this.state.imageUrl} alt="" />
+        <span className={this.getBatchClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-m">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBatchClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
+
+export default Counter;
