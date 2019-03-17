cb7f7d9dcb02608ee95cb0fd4d883ee2edfa215b
Anushan Tennakoon <anushan.tennakoon@rxpservices.com>
2019-03-17 22:42:40 +1100

    Conditional operator

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,22 +11,39 @@ class Counter extends Component {
     fontSize: 60,
     fontWeight: "bold"
   };
-  render() {
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
     return (
-      <React.Fragment>
-        <img src={this.state.imageUrl} alt="" />
-        <span className={this.getBatchClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-m">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getBatchClasses() {
+  render() {
+    // return (
+    /*       <React.Fragment>
+        <img src={this.state.imageUrl} alt="" />
+        <span className={this.getBatchClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-m">Increment</button>
+      </React.Fragment> */
+    // );
+
+    //return <div>{this.renderTags()}</div>;
+
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+      </div>
+    );
+  }
+
+  /*   getBatchClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
@@ -35,7 +52,7 @@ class Counter extends Component {
   formatCount() {
     const { count } = this.state;
     return count === 0 ? <h1>Zero</h1> : count;
-  }
+  } */
 }
 
 export default Counter;
